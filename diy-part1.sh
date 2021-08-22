49537deaae2526cb6ffd6b282015ea5cca56dce7
zhangyanfeiaa <71303082+zhangyanfeiaa@users.noreply.github.com>
2021-08-22 12:44:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,3 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git destan19 https://github.com/destan19/OpenAppFilter.git' feeds.conf.default
-git clone https://github.com/destan19/OpenAppFilter.git
